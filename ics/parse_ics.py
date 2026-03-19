a6b9fb8e04b07b01748c42efab064b29e2a42a53
dbm1982 <dbm1982@Hotmail.com>
2026-03-18 23:22:07 -0400

    Update parse_ics.py

--- ics/parse_ics.py
+++ ics/parse_ics.py
@@ -1,1 +1,21 @@
+from ics import Calendar
+from datetime import datetime
 
+def parse_ics(ics_text: str):
+    """
+    Parses ICS text and returns a list of event dicts.
+    """
+    cal = Calendar(ics_text)
+    events = []
+
+    for e in cal.events:
+        events.append({
+            "event_id": e.uid,
+            "start": e.begin.datetime,
+            "end": e.end.datetime,
+            "summary": e.name,
+            "location": e.location,
+            "description": e.description,
+        })
+
+    return events
